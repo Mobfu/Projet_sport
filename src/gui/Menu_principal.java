8136ad23d186e9fa7d169974c2221e5371f36aca
tanguy <tanguy.moritz@groupe-esigelec.org>
2024-04-11 14:59:53 +0200

    2eme push

--- src/gui/Menu_principal.java
+++ src/gui/Menu_principal.java
@@ -19,7 +19,7 @@ import javax.swing.border.EmptyBorder;
 public class Menu_principal extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
-	private JButton btnDeconnexion, btnGestionClub, btnAffichageClub, btnAffichageUtilisateurs, btnGestionUtilisateurs;
+	private JButton btnDeconnexion, btnHistoriqueConnexion, btnAffichageClub, btnAffichageUtilisateurs, btnGestionUtilisateurs;
 	/**
 	 * Launch the application.
 	 */
@@ -52,29 +52,43 @@ public class Menu_principal extends JFrame implements ActionListener{
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("/fond.jpg")).getImage();
 		
+		JLabel lblGestionsConnexionsEt = new JLabel("gestions connexions et profils");
+		lblGestionsConnexionsEt.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGestionsConnexionsEt.setForeground(Color.WHITE);
+		lblGestionsConnexionsEt.setFont(new Font("Bahnschrift", Font.PLAIN, 25));
+		lblGestionsConnexionsEt.setBounds(299, 175, 355, 37);
+		contentPane.add(lblGestionsConnexionsEt);
+		
+		JLabel lblConsulterDesInformations = new JLabel("consulter des informations");
+		lblConsulterDesInformations.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConsulterDesInformations.setForeground(Color.WHITE);
+		lblConsulterDesInformations.setFont(new Font("Bahnschrift", Font.PLAIN, 25));
+		lblConsulterDesInformations.setBounds(24, 56, 355, 37);
+		contentPane.add(lblConsulterDesInformations);
+		
 		//JButtons
 		
-		this.btnGestionClub = new JButton("gestion de clubs");
-		btnGestionClub.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
-		btnGestionClub.setBounds(394, 232, 251, 30);
-		contentPane.add(btnGestionClub);
-		btnGestionClub.addActionListener(this);
+		this.btnHistoriqueConnexion = new JButton("historique des connexion");
+		btnHistoriqueConnexion.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
+		btnHistoriqueConnexion.setBounds(299, 218, 355, 30);
+		contentPane.add(btnHistoriqueConnexion);
+		btnHistoriqueConnexion.addActionListener(this);
 		
 		this.btnAffichageClub = new JButton("afficher les clubs de sport");
 		btnAffichageClub.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
-		btnAffichageClub.setBounds(10, 232, 355, 30);
+		btnAffichageClub.setBounds(24, 134, 355, 30);
 		contentPane.add(btnAffichageClub);
 		btnAffichageClub.addActionListener(this);
 		
 		this.btnAffichageUtilisateurs = new JButton("afficher une liste d'utilisateurs");
 		btnAffichageUtilisateurs.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
-		btnAffichageUtilisateurs.setBounds(10, 125, 355, 30);
+		btnAffichageUtilisateurs.setBounds(24, 93, 355, 30);
 		contentPane.add(btnAffichageUtilisateurs);
 		btnAffichageUtilisateurs.addActionListener(this);
 		
 		this.btnGestionUtilisateurs = new JButton("gérer les utilisateurs");
 		btnGestionUtilisateurs.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
-		btnGestionUtilisateurs.setBounds(394, 125, 251, 30);
+		btnGestionUtilisateurs.setBounds(299, 259, 355, 30);
 		contentPane.add(btnGestionUtilisateurs);
 		btnGestionUtilisateurs.addActionListener(this);
 		
@@ -112,7 +126,7 @@ public class Menu_principal extends JFrame implements ActionListener{
 			Page_connexion frame = new Page_connexion();
 			frame.setVisible(true);
 			dispose();
-		}else if(ae.getSource()==btnGestionClub) {
+		}else if(ae.getSource()==btnHistoriqueConnexion) {
 			System.out.println("page pas encre créée");
 			dispose();
 		}
@@ -131,5 +145,4 @@ public class Menu_principal extends JFrame implements ActionListener{
 			dispose();
 		}		
 	}
-
 }
