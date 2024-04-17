ef68e39ea28f7fb022713aeca9c5265dbd03e335
jojooooooooo20 <147375526+jojooooooooo20@users.noreply.github.com>
2024-04-17 16:52:43 +0200

    Update Filtrer_profils.java
    By J

--- src/main/java/gui/Filtrer_profils.java
+++ src/main/java/gui/Filtrer_profils.java
@@ -1,8 +1,7 @@
 package gui;
-
+ 
 import java.awt.EventQueue;
 import java.awt.Image;
-import dao.DBDAO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -16,83 +15,70 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 import java.awt.Window;
 import java.awt.event.*;
 import javax.swing.*;
-
-public class Filtrer_clubs extends JFrame implements ActionListener{
-
+ 
+public class Filtrer_profils extends JFrame implements ActionListener{
+ 
 	private JPanel contentPane;
-	private JTextField textField;
 	private JTextField textField_1;
 	private JButton btnAnnuler, btnAppliquer;
-	
-	public Filtrer_clubs() {
-		
+	public Filtrer_profils() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 410);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
 		//recuperation de l image depuis le fichier
-		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("fond.jpg")).getImage();
-		
+
+		String[] choix = {"administrateur", "élu", "acteurs du monde sportif"};
+		JComboBox<String> comboBox = new JComboBox<>(choix);
+		comboBox.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
+		comboBox.setMaximumRowCount(3);
+		comboBox.setBounds(279, 110, 335, 30);
+
+ 
+		contentPane.add(comboBox);
 		//JLButtons
-		
 		this.btnAnnuler = new JButton("annuler");
 		btnAnnuler.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAnnuler.setBounds(126, 323, 140, 30);
 		contentPane.add(btnAnnuler);
 		btnAnnuler.addActionListener(this);
-		
 		this.btnAppliquer = new JButton("appliquer filtre");
 		btnAppliquer.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAppliquer.setBounds(381, 323, 222, 30);
 		contentPane.add(btnAppliquer);
 		btnAppliquer.addActionListener(this);
-		
 		//JTextfields
-		
 		textField_1 = new JTextField();
-		textField_1.setBounds(454, 197, 222, 30);
+		textField_1.setBounds(279, 198, 335, 30);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
-		textField = new JTextField();
-		textField.setBounds(454, 114, 222, 30);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
 		//JLabels
-		
-		JLabel lblNewLabel_2 = new JLabel("lieu (commune, région, département) :");
+		JLabel lblNewLabel_2 = new JLabel("nom d'utilisateur :");
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		lblNewLabel_2.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
-		lblNewLabel_2.setBounds(54, 198, 375, 27);
+		lblNewLabel_2.setBounds(55, 199, 222, 27);
 		contentPane.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_1 = new JLabel("Nom de fédération :");
+		JLabel lblNewLabel_1 = new JLabel("rôle :");
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		lblNewLabel_1.setBackground(new Color(240, 240, 240));
 		lblNewLabel_1.setLabelFor(this);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(54, 115, 205, 27);
+		lblNewLabel_1.setBounds(55, 112, 176, 27);
 		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel = new JLabel("Filtrer les clubs de sport");
+		JLabel lblNewLabel = new JLabel("Filtrer les profils utilisateurs");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setBounds(165, 8, 347, 37);
+		lblNewLabel.setBounds(149, 8, 417, 37);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Bahnschrift", Font.PLAIN, 30));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		
 		//insertion de l image dans un JLabel, attention dernier JLabel declare pour qu il reste en fond
-		
 		JLabel imageLabel = new JLabel("");
 		imageLabel.setToolTipText("");
 		imageLabel.setLabelFor(this);
@@ -101,9 +87,13 @@ public class Filtrer_clubs extends JFrame implements ActionListener{
 		imageLabel.setIcon(new ImageIcon(background));
 		contentPane.add(imageLabel);
 		imageLabel.setBounds(0, 0, 686, 373);
+		JLabel lblNewLabel_2_2 = new JLabel("rôle ");
+		lblNewLabel_2_2.setForeground(Color.WHITE);
+		lblNewLabel_2_2.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
+		lblNewLabel_2_2.setBounds(198, 237, 79, 27);
+		contentPane.add(lblNewLabel_2_2);
 		setLocationRelativeTo(null);
 	}
-	
 	/**
 	 * Launch the application.
 	 */
@@ -111,7 +101,7 @@ public class Filtrer_clubs extends JFrame implements ActionListener{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Filtrer_clubs frame = new Filtrer_clubs();
+					Filtrer_profils frame = new Filtrer_profils();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -119,26 +109,17 @@ public class Filtrer_clubs extends JFrame implements ActionListener{
 			}
 		});
 	}
-
+ 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
-	    if(ae.getSource()==btnAppliquer) {
-	        String federationName = textField.getText();
-	        String location = textField_1.getText();
-
-	        DBDAO dbdao = new DBDAO();
-	        List<String> results = dbdao.searchClubsByFederationAndLocation(federationName, location);
-
-	        // Affichage des résultats dans une nouvelle fenêtre
-	        Resultats_recherche_club resultat = new Resultats_recherche_club(results);
-	        resultat.setVisible(true);
-
-	        dispose();
-	    } else if(ae.getSource()==btnAnnuler) {
-	        Liste_clubs frame = new Liste_clubs();
-	        frame.setVisible(true);
-	        dispose();
-	    }
+		if(ae.getSource()==btnAppliquer) {
+			Liste_utilisateurs frame = new Liste_utilisateurs ();
+			frame.setVisible(true);
+			dispose();
+		}else if(ae.getSource()==btnAnnuler) {
+			Liste_utilisateurs frame = new Liste_utilisateurs ();
+			frame.setVisible(true);
+			dispose();
+		}
 	}
-
 }
