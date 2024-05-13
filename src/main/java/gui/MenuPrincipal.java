194e100754c45498d4a18af46140fd0bef0cfd3d
leturbotangz <148070492+leturbotangz@users.noreply.github.com>
2024-05-13 15:30:25 +0200

    Update and rename Menu_principal.java to MenuPrincipal.java

--- src/main/java/gui/MenuPrincipal.java
+++ src/main/java/gui/MenuPrincipal.java
@@ -16,7 +16,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
  
-public class Menu_principal extends JFrame implements ActionListener{
+public class MenuPrincipal extends JFrame implements ActionListener{
 	 
 	private JPanel contentPane;
 	private JButton btnDeconnexion, btnHistoriqueConnexion, btnAffichageClub, btnAffichageUtilisateurs, btnGestionUtilisateurs;
@@ -27,7 +27,7 @@ public class Menu_principal extends JFrame implements ActionListener{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Menu_principal frame = new Menu_principal();
+					MenuPrincipal frame = new MenuPrincipal();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +39,7 @@ public class Menu_principal extends JFrame implements ActionListener{
 	/**
 	 * Create the frame.
 	 */
-	public Menu_principal() {
+	public MenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 410);
 		contentPane = new JPanel();
@@ -49,40 +49,40 @@ public class Menu_principal extends JFrame implements ActionListener{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("fond.jpg")).getImage();
-		JLabel lblGestionsConnexionsEt = new JLabel("gestions connexions et profils");
+		JLabel lblGestionsConnexionsEt = new JLabel("Gestion connexions et profils");
 		lblGestionsConnexionsEt.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGestionsConnexionsEt.setForeground(Color.WHITE);
 		lblGestionsConnexionsEt.setFont(new Font("Bahnschrift", Font.PLAIN, 25));
 		lblGestionsConnexionsEt.setBounds(299, 175, 355, 37);
 		contentPane.add(lblGestionsConnexionsEt);
-		JLabel lblConsulterDesInformations = new JLabel("consulter des informations");
+		JLabel lblConsulterDesInformations = new JLabel("Consulter des informations");
 		lblConsulterDesInformations.setHorizontalAlignment(SwingConstants.CENTER);
 		lblConsulterDesInformations.setForeground(Color.WHITE);
 		lblConsulterDesInformations.setFont(new Font("Bahnschrift", Font.PLAIN, 25));
 		lblConsulterDesInformations.setBounds(24, 56, 355, 37);
 		contentPane.add(lblConsulterDesInformations);
 		//JButtons
-		this.btnHistoriqueConnexion = new JButton("historique des connexion");
+		this.btnHistoriqueConnexion = new JButton("Historique des connexions");
 		btnHistoriqueConnexion.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnHistoriqueConnexion.setBounds(299, 218, 355, 30);
 		contentPane.add(btnHistoriqueConnexion);
 		btnHistoriqueConnexion.addActionListener(this);
-		this.btnAffichageClub = new JButton("afficher les clubs de sport");
+		this.btnAffichageClub = new JButton("Afficher les clubs de sport");
 		btnAffichageClub.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAffichageClub.setBounds(24, 134, 355, 30);
 		contentPane.add(btnAffichageClub);
 		btnAffichageClub.addActionListener(this);
-		this.btnAffichageUtilisateurs = new JButton("afficher une liste d'utilisateurs");
+		this.btnAffichageUtilisateurs = new JButton("Afficher une liste d'utilisateurs");
 		btnAffichageUtilisateurs.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAffichageUtilisateurs.setBounds(24, 93, 355, 30);
 		contentPane.add(btnAffichageUtilisateurs);
 		btnAffichageUtilisateurs.addActionListener(this);
-		this.btnGestionUtilisateurs = new JButton("gérer les utilisateurs");
+		this.btnGestionUtilisateurs = new JButton("Gérer les utilisateurs");
 		btnGestionUtilisateurs.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnGestionUtilisateurs.setBounds(299, 259, 355, 30);
 		contentPane.add(btnGestionUtilisateurs);
 		btnGestionUtilisateurs.addActionListener(this);
-		this.btnDeconnexion = new JButton("déconnexion");
+		this.btnDeconnexion = new JButton("Déconnexion");
 		btnDeconnexion.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnDeconnexion.setBounds(261, 312, 163, 30);
 		contentPane.add(btnDeconnexion);
@@ -110,26 +110,26 @@ public class Menu_principal extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if(ae.getSource()==btnDeconnexion) {
-			Page_Connexion frame = new Page_Connexion();
+			PageConnexion frame = new PageConnexion();
 			frame.setVisible(true);
 			dispose();
 		}else if(ae.getSource()==btnHistoriqueConnexion) {
-			Historique_connexions frame = new Historique_connexions();
+			HistoriqueConnexions frame = new HistoriqueConnexions();
 			frame.setVisible(true);
 			dispose();
 		}
 		else if(ae.getSource()==btnAffichageClub) {
-			Liste_utilisateurs frame = new Liste_utilisateurs();
+			ListeClubs frame = new ListeClubs();
 			frame.setVisible(true);
 			dispose();
 				}
 		else if(ae.getSource()==btnAffichageUtilisateurs) {
-			Liste_utilisateurs frame = new Liste_utilisateurs();
+			ListeUtilisateurs frame = new ListeUtilisateurs();
 			frame.setVisible(true);
 			dispose();
 		}
 		else if(ae.getSource()==btnGestionUtilisateurs) {
-			Gestion_utilisateurs frame = new Gestion_utilisateurs();
+			GestionUtilisateurs frame = new GestionUtilisateurs();
 			frame.setVisible(true);
 			dispose();
 		}		
