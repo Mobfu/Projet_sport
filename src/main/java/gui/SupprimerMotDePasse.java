98a7b892eb4de431705913efcf5ecf9f560a4581
leturbotangz <148070492+leturbotangz@users.noreply.github.com>
2024-05-13 15:33:00 +0200

    Update and rename SuppUserMdp.java to SupprimerMotDePasse.java

--- src/main/java/gui/SupprimerMotDePasse.java
+++ src/main/java/gui/SupprimerMotDePasse.java
@@ -19,7 +19,7 @@ import java.awt.Window;
 import java.awt.event.*;
 import javax.swing.*;
  
-public class SuppUserMdp extends JFrame implements ActionListener{
+public class SupprimerMotDePasse extends JFrame implements ActionListener{
  
 	private DBDAO dbdao ;
 	private JPanel contentPane;
@@ -27,7 +27,7 @@ public class SuppUserMdp extends JFrame implements ActionListener{
 	private JTextField textField_2;
 	private JLabel lblAfinDeSupprimer;
 	
-	public SuppUserMdp() {
+	public SupprimerMotDePasse() {
 		dbdao = new DBDAO();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 410);
@@ -40,11 +40,11 @@ public class SuppUserMdp extends JFrame implements ActionListener{
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("fond.jpg")).getImage();
 		
-		lblAfinDeSupprimer = new JLabel("Afin de supprimer un utilisateur seul son username suffit");
+		lblAfinDeSupprimer = new JLabel("Afin de supprimer un mot de passe le nom de l'utilisateur suffit");
 		lblAfinDeSupprimer.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAfinDeSupprimer.setForeground(Color.WHITE);
-		lblAfinDeSupprimer.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
-		lblAfinDeSupprimer.setBounds(83, 127, 543, 37);
+		lblAfinDeSupprimer.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
+		lblAfinDeSupprimer.setBounds(10, 127, 666, 37);
 		contentPane.add(lblAfinDeSupprimer);
 		
 		textField_2 = new JTextField();
@@ -54,30 +54,30 @@ public class SuppUserMdp extends JFrame implements ActionListener{
 		
 		//JLButtons
 		
-		this.btnAnnuler = new JButton("annuler");
+		this.btnAnnuler = new JButton("Annuler");
 		btnAnnuler.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAnnuler.setBounds(148, 323, 140, 30);
 		contentPane.add(btnAnnuler);
 		btnAnnuler.addActionListener(this);
 		
-		this.btnAjouter = new JButton("supprimer");
+		this.btnAjouter = new JButton("Supprimer");
 		btnAjouter.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAjouter.setBounds(428, 323, 140, 30);
 		contentPane.add(btnAjouter);
 		btnAjouter.addActionListener(this);
 		
-		JLabel lblNewLabel_1 = new JLabel("Username :");
+		JLabel lblNewLabel_1 = new JLabel("Nom de l'utilisateur :");
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		lblNewLabel_1.setBackground(new Color(240, 240, 240));
 		lblNewLabel_1.setLabelFor(this);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_1.setBounds(148, 174, 135, 30);
+		lblNewLabel_1.setBounds(67, 174, 216, 30);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel = new JLabel("Supprimer mot de passe d'un utilisateur");
+		JLabel lblNewLabel = new JLabel("Supprimer  le mot de passe d'un utilisateur");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setBounds(165, 8, 347, 37);
+		lblNewLabel.setBounds(10, 8, 666, 37);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Bahnschrift", Font.PLAIN, 30));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -102,7 +102,7 @@ public class SuppUserMdp extends JFrame implements ActionListener{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					SuppUserMdp frame = new SuppUserMdp();
+					SupprimerMotDePasse frame = new SupprimerMotDePasse();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -117,11 +117,11 @@ public class SuppUserMdp extends JFrame implements ActionListener{
 			String name = textField_2.getText();
 			dbdao.deleteUserPassword(name);
 			JOptionPane.showMessageDialog(null, "Mot de passe de l'utilisateur supprimé avec succès !", "Succès", JOptionPane.INFORMATION_MESSAGE);
-			Menu_principal frame=new Menu_principal();
+			MenuPrincipal frame=new MenuPrincipal();
 			frame.setVisible(true);
 			dispose();
 		}else if(ae.getSource()==btnAnnuler) {
-			Gestion_utilisateurs frame = new Gestion_utilisateurs();
+			GestionUtilisateurs frame = new GestionUtilisateurs();
 			frame.setVisible(true);			
 			dispose();
 		}
