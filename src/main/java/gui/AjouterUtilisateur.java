0ea35e7c30e8aee3d4d1d3b685913ef423eb5ce1
jojooooooooo20 <147375526+jojooooooooo20@users.noreply.github.com>
2024-05-29 14:49:48 +0200

    Update AjouterUtilisateur.java

--- src/main/java/gui/AjouterUtilisateur.java
+++ src/main/java/gui/AjouterUtilisateur.java
@@ -1,13 +1,12 @@
 package gui;
 import java.awt.EventQueue;
- 
 import java.awt.Image;
-
+ 
 import javax.swing.border.EmptyBorder;
-
+ 
 import Module.Utilisateur;
 import dao.DBDAO;
-
+ 
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -18,9 +17,7 @@ import java.util.List;
 import java.awt.Window;
 import java.awt.event.*;
 import javax.swing.*;
- 
 public class AjouterUtilisateur extends JFrame implements ActionListener{
- 
 	private DBDAO dbdao ;
 	private JPanel contentPane;
 	private JPasswordField textField_1;
@@ -28,29 +25,22 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 	private JTextField textField;
 	private JTextField textField_2;
 	private boolean MDPVisible=false;
-	
 	public AjouterUtilisateur() {
 		dbdao = new DBDAO();
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 410);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
 		//recuperation de l image depuis le fichier
-		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		Image background = new ImageIcon(this.getClass().getResource("fond.jpg")).getImage();
-		
 		// Charger et redimensionner l'image
-				
 		ImageIcon imageDeBase = new ImageIcon(getClass().getResource("/gui/eye.png"));
 		Image oeilimage = imageDeBase.getImage().getScaledInstance(40, 23, Image.SCALE_SMOOTH);
 		ImageIcon oeil2 = new ImageIcon(oeilimage);
-
+ 
 		// Créer le bouton avec l'icône redimensionnée
-		
 		this.icone = new JButton(oeil2);
 		icone.setBounds(578, 213, 31, 30);
 		contentPane.add(icone);
@@ -59,12 +49,10 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 		textField_2.setBounds(305, 95, 263, 30);
 		contentPane.add(textField_2);
 		icone.addActionListener(this);
-		
 		textField = new JTextField();
 		textField.setColumns(10);
 		textField.setBounds(305, 152, 263, 30);
 		contentPane.add(textField);
-		
 		JLabel lblNewLabel_1_1 = new JLabel("Email :");
 		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_1.setForeground(Color.WHITE);
@@ -72,37 +60,29 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 		lblNewLabel_1_1.setBackground(UIManager.getColor("Button.background"));
 		lblNewLabel_1_1.setBounds(179, 151, 76, 30);
 		contentPane.add(lblNewLabel_1_1);
-		
 		//JLButtons
-		
 		this.btnAnnuler = new JButton("Annuler");
 		btnAnnuler.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAnnuler.setBounds(148, 323, 140, 30);
 		contentPane.add(btnAnnuler);
 		btnAnnuler.addActionListener(this);
-		
 		this.btnAjouter = new JButton("Ajouter");
 		btnAjouter.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		btnAjouter.setBounds(428, 323, 140, 30);
 		contentPane.add(btnAjouter);
 		btnAjouter.addActionListener(this);
-		
 		//JTextfields
-		
 		textField_1 = new JPasswordField();
 		textField_1.setBounds(305, 213, 263, 30);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
 		//JLabels
-		
 		JLabel lblNewLabel_2 = new JLabel("Mot de passe :");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		lblNewLabel_2.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
 		lblNewLabel_2.setBounds(102, 212, 158, 30);
 		contentPane.add(lblNewLabel_2);
-		
 		JLabel lblNewLabel_1 = new JLabel("Nom d'utilisateur :");
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Bahnschrift", Font.PLAIN, 22));
@@ -111,16 +91,13 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1.setBounds(69, 94, 199, 30);
 		contentPane.add(lblNewLabel_1);
-		
 		JLabel lblNewLabel = new JLabel("Ajouter un utilisateur");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setBounds(165, 8, 347, 37);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Bahnschrift", Font.PLAIN, 30));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		
 		//insertion de l image dans un JLabel, attention dernier JLabel declare pour qu il reste en fond
-		
 		JLabel imageLabel = new JLabel("");
 		imageLabel.setToolTipText("");
 		imageLabel.setLabelFor(this);
@@ -131,12 +108,10 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 		imageLabel.setBounds(0, 0, 686, 373);
 		setLocationRelativeTo(null);
 	}
-	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -148,7 +123,6 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 			}
 		});
 	}
- 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 	    if(ae.getSource()==btnAjouter) {
@@ -158,7 +132,9 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 	        if(email.contains("@")) {
 	        	try {
 		        	if(dbdao.addUser(name, email, password)){
-			            JOptionPane.showMessageDialog(null, "Utilisateur ajouté avec succès !", "Succès", JOptionPane.INFORMATION_MESSAGE);
+			            String type_action="ajout d'un utilisateur";
+		        		JOptionPane.showMessageDialog(null, "Utilisateur ajouté avec succès !", "Succès", JOptionPane.INFORMATION_MESSAGE);
+			            dbdao.AjoutAction(type_action, PageConnexion.session);
 			            GestionUtilisateurs frame = new GestionUtilisateurs();
 			            frame.setVisible(true);
 			            dispose();
@@ -190,6 +166,5 @@ public class AjouterUtilisateur extends JFrame implements ActionListener{
 	        	 MDPVisible = true;
 	         }
 	    }
-	    
 	}
 }
